f5621501d26c5abb61440b067331741817dceaaa
Shankk <brianviveiros@hotmail.com>
2026-03-17 20:44:54 -0400

    created login page. added logo and login images.

--- src/components/content/Auth.jsx
+++ src/components/content/Auth.jsx
@@ -1,18 +1,25 @@
-
+import logo from '../../assets/cat-logo.png'
+import front from '../../assets/front-social.png'
+import '../style/Auth.css'
 
 function LogInForm() {
 
     return (
         <>
             <p>Log in to Catstagram</p>
-            <form action="/log-in" method="POST">
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" />
-                <button>Log in</button>
+            <form className='authForm' action="/log-in" method="POST">
+                <div className='input-container'>
+                    <input className='authInput' type="text" name="username" placeholder=' '/>
+                    <label className='authLabel' htmlFor="username">Username or email address</label>
+                </div>
+                <div className='input-container'>
+                    <input className='authInput' type="text" name="password" placeholder=' ' />
+                    <label className='authLabel' htmlFor="password">Password</label>
+                </div>
+                <button className='form-button' href="">Log in</button>
             </form>
-            <a href="/sign-up">Sign Up</a>
+            <button className='form-button'>Forgot password?</button>
+            <button className='form-button' href="/sign-up">Create new account</button>
         </>
     );
 }
@@ -20,11 +27,9 @@ function SignUpForm() {
     return (
         <>
             <p>Sign Up to Catstagram</p>
-            <form action="/sign-up" method="POST">
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" />
+            <form className='authForm' action="/sign-up" method="POST">
+                <input className='authInput' type="text" name="username" />
+                <input className='authInput' type="text" name="password" placeholder='Password' />
                 <button>Sign Up</button>
             </form>
             <a href="/log-in">Sign Up</a>
@@ -35,21 +40,27 @@ function SignUpForm() {
 function LogInPage() {
 
     return (
-        <div>
-            <div className="left-side">
-                <div className="container">
-                    <img className="logo" src="" alt="" />
+        <div className='page'>
+            <div className='content'>
+                <div className="left-side">
+                    <div className="container">
+                        <img className="logo" src={logo} alt="" />
+                    </div>
+                    <div className="container">
+                        <p className='front-text'>See everyday moments from your <span className='fancy-text'>close friends</span>.</p>
+                    </div>
+                    <div className="container">
+                        <img className='front' src={front} alt="" />
+                    </div>
                 </div>
-                <div className="container">
-                    <p>See everyday moments from your close friends.</p>
-                </div>
-                <div className="container">
-                    <img src="" alt="" />
+                <div className="right-side">
+                    <LogInForm ></LogInForm>
                 </div>
             </div>
-            <div className="right-side">
-                <LogInForm></LogInForm>
+            <div className='footer'>
+                <p>2026 Catstagram</p>
             </div>
+            
         </div>
     )
 }
